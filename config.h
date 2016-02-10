b869c13a6dd420a6f7686292ce7db28d4988edd0
Micah N Gorrell <micahg@netmail.com>
2016-02-10 11:37:27 -0700

    Fixed word delimiters

--- config.h
+++ config.h
@@ -34,7 +34,7 @@ static float chscale = 1.0;
  *
  * More advanced example: " `'\"()[]{}"
  */
-static char worddelimiters[] = " ";
+static char worddelimiters[] = " `'\"()[]{}<>\t~!@#$%^&*-=\\/;:,.?|";
 
 /* selection timeouts (in milliseconds) */
 static unsigned int doubleclicktimeout = 300;
