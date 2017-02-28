6d681423f428cb2908b3780c7c4d95c69e7f6146
Micah N Gorrell <micahg@netmail.com>
2017-02-28 00:45:14 -0700

    Tweaking styles a bit

--- config.h
+++ config.h
@@ -6,7 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 // static char font[] = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static char font[] = "ProggyTinyTT:size=16:antialias=false:autohint=false";
+// static char font[] = "ProggyTinyTT:size=16:antialias=false:autohint=false";
+static char font[] = "xos4 Terminus:size=10:antialias=false:autohint=false";
 static int borderpx = 2;
 static int histsize = 2000;
 
